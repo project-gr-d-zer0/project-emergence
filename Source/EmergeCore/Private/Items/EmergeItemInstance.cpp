87f2e21ad1f796dfc060a989a66fa60b194da618
emerge-builder <builder@rick>
2026-07-08 00:56:43 -0700

    builder[item-durability]: attempt 2 (Source/EmergeCore/Public/Items/EmergeItemInstance.h, Source/EmergeCore/Private/Items/EmergeItemInstance.cpp)

--- Source/EmergeCore/Private/Items/EmergeItemInstance.cpp
+++ Source/EmergeCore/Private/Items/EmergeItemInstance.cpp
@@ -1,6 +1,5 @@
 #include "EmergeItemInstance.h"
 
-// Implementation of IsBroken Method
 bool UEmergeItemInstance::IsBroken(float currentDurability) const {
     return currentDurability <= 0.0f;
 }
--- Source/EmergeCore/Public/Items/EmergeItemInstance.h
+++ Source/EmergeCore/Public/Items/EmergeItemInstance.h
@@ -1,19 +1,17 @@
 #pragma once
+
 #include "CoreMinimal.h"
 #include "UObject/Object.h"
 #include "EmergeItemInstance.generated.h"
 
-// This class represents an instance of an item in the game.
 UCLASS()
 class EMERGECORE_API UEmergeItemInstance : public UObject
 {
 	GENERATED_BODY()
 
 public:
-	// Current durability of the item.
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Durability")
 	float CurrentDurability;
 
-	// New Method to Check if Item is Broken
 	bool IsBroken(float currentDurability) const;
 };
